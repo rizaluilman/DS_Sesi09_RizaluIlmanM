096d7bf8fecd869ef2b365b8266171147f3e41d4
Rizalu Ilman M <47112673+rizaluilman@users.noreply.github.com>
2024-02-27 21:34:46 +0700

    update test case login

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -1,15 +1,39 @@
 const { $, expect } = require('@wdio/globals')
 const Page = require('./page');
+const loginPage = require('../pageobjects/login.page');
 
-class HomePage extends Page {
-    get iconCart () { return $('.shopping_cart_link'); }
+class HomePage extends Page{
+    get iconCart () { return $('.shopping_cart_link') }
+    get addBackpackToCart () { return $('#add-to-cart-sauce-labs-backpack') }
+    get badgeCart () { return $('.shopping_cart_badge') }
 
     async validateHomePage() {
-        expect(browser).toHaveUrlContaining('/invetory.html')
-        expect(this.iconCart).toBeDisplayed()
+        await expect(browser).toHaveUrlContaining('/inventory.html')
+        await expect(this.iconCart).toBeDisplayed()
     }
+
+    //Login With StandardUser
+    async loginStandarUser(){
+        await loginPage.open()
+        await loginPage.login(process.env.USERNAME_STANDARD_USER, process.env.PASSWORD_SAUCEDEMO)
+    }
+    //
+
+    async addAProductToCart(){
+        await this.loginStandarUser()
+        await this.addBackpackToCart.waitForDisplayed({ timeout : 3000})
+        await this.addBackpackToCart.click()
+    }
+
+    async validateBadgeCart(){
+        
+        await this.badgeCart.waitForDisplayed({ timeout : 3000 })
+        await expect(this.badgeCart).toBeDisplayed()
+    }
+
     open () {
         return super.open('/inventory.html');
     }
 }
+
 module.exports = new HomePage();
--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,24 +1,44 @@
-const { $, expect } = require('@wdio/globals')
+const { $ } = require('@wdio/globals')
 const Page = require('./page');
 
 class LoginPage extends Page {
-    get fieldUsername () { return $('#user-name'); }
-    get fieldPassword () { return $('#password'); }
-    get buttonLogin () { return $('#login-button'); }
-    get errorLockedOutUser () { return $('//h3[text()="Epic sadface: Sorry, this user has been locked out."]') }
+    get fieldUsername () {return $('#user-name');}
+    get fieldPassword () {return $('#password');}
+    get btnLogin () {return $('#login-button');}
+    get errorLockedUser() { return $('//h3[text()="Epic sadface: Sorry, this user has been locked out."]')}
+    get errorNotRegisterdUser() { return $('//h3[text()="Epic sadface: Username and password do not match any user in this service"]')}
+    get errorBlankUsername() { return $('//h3[text()="Epic sadface: Username is required"]')}
+    get errorBlankPassword() { return $('//h3[text()="Epic sadface: Password is required"]')}
 
     async login (username, password) {
-        await this.fieldUsername.waitForDisplayed({ timeout: 2500 });
+        await this.fieldUsername.waitForDisplayed({ timeout : 3000})
         await this.fieldUsername.setValue(username);
         await this.fieldPassword.setValue(password);
-        await this.buttonLogin.click();
+        await this.btnLogin.click();
     }
-    async validateLockedOutUserError () {
-        await this.errorLockedOutUser.waitForDisplayed({ timeout: 2500 });
-        await expect(this.errorLockedOutUser).toBeDisplayed()
+
+    async validateLockedUser(){
+        await this.errorLockedUser.waitForDisplayed({ timeout : 3000})
+        await expect(this.errorLockedUser).toBeDisplayed()
     }
+
+    async validateNotRegisteredUser(){
+        await this.errorNotRegisterdUser.waitForDisplayed({ timeout : 3000})
+        await expect(this.errorNotRegisterdUser).toBeDisplayed()
+    }
+
+    async validateBlankUsername(){
+        await this.errorBlankUsername.waitForDisplayed({ timeout : 3000})
+        await expect(this.errorBlankUsername).toBeDisplayed()
+    }
+
+    async validateBlankPassword(){
+        await this.errorBlankPassword.waitForDisplayed({ timeout : 3000})
+        await expect(this.errorBlankPassword).toBeDisplayed()
+    }
+
     open () {
-        return super.open('/'); 
+        return super.open('/');
     }
 }
 
